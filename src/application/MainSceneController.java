fba4f7faae2c590a8fcaf2c72f1155932eacae9f
Jason Nickell <43504239+data10019@users.noreply.github.com>
2022-03-02 02:13:01 -0800

    Added button labels
    added some controller code

--- src/application/MainSceneController.java
+++ src/application/MainSceneController.java
@@ -1,5 +1,15 @@
 package application;
 
-public class MainSceneController {
-	
+import javafx.scene.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.fxml.*;
+import javafx.event.*;
+import javafx.stage.*;
+import java.io.IOException;
+
+public class MainSceneController 
+{
+	@FXML
+	private Button StudentButton;
 }
